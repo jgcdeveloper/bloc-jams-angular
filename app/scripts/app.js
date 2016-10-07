1228c9fa98bae5a4d711b31d2424f1407d0281db
Jason Clegg <jgcdeveloper@gmail.com>
2016-10-07 10:54:39 +0500

    Added template pages for home, landing, collection, and album

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,8 +1,26 @@
 //Opinionated style guide recommends wrapping in a function to avoid pollution of global namespace
 (function () { 
 
+    function config ($stateProvider,$locationProvider) {
+        $locationProvider
+            .html5Mode({
+                enabled: true;
+                requireBase: false;
+        });
+    
+    $stateProvider
+        .state('landing', {
+            url: '/',
+            templateURL: '/templates/landing.html'
+    })
+        .state('album', {
+            url: '/album/',
+            templateURL: '/templates/album.html'
+    });
+    
+    }
 //'blocJams' is the name of the module, [] is for dependancy injection.
-angular
-    .module('blocJams',[]);
-
+    angular
+        .module('blocJams', ['ui.router']);
+        .config(config);
 })();
