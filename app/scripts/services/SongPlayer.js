d5d9475be08d553656eca4b68a238ec56a3070f4
Jason Clegg <jgcdeveloper@gmail.com>
2016-10-10 22:03:25 +0500

    Checkpoint 07 Complete - Added SongPlayer factory service to Bloc-Jams. Modified album.html to accomidate

---
+++ app/scripts/services/SongPlayer.js
@@ -0,0 +1,72 @@
+(function() {
+    function SongPlayer() {
+               
+        /**
+        * @desc SongPlayer constructor
+        * @type {Object}
+        */
+        var SongPlayer = {},
+            
+        /**
+        * @desc currentSong selected
+        * @type {Object}
+        */
+        currentSong = null,
+            
+        /**
+        * @desc Buzz object audio file
+        * @type {Object}
+        */
+        currentBuzzObject = null;
+        
+        /*
+        * @function setSong
+        * @desc Stops currently playing song and loads new audio file as currentBuzzObject
+        * @param {Object} song
+        */
+        var setSong = function(song){
+            
+            if (currentBuzzObject) {
+                currentBuzzObject.stop();
+                currentSong.playing = null;
+            }
+ 
+            currentBuzzObject = new buzz.sound(song.audioUrl, {
+                formats: ['mp3'],
+                preload: true
+            });
+ 
+            currentSong = song;
+                
+        };
+        
+        SongPlayer.play = function(song){
+            
+            if (currentSong !== song) {    
+                setSong(song);
+                currentBuzzObject.play();
+                song.playing = true;
+            
+            } else if(currentSong === song) {
+            
+                if (currentBuzzObject.isPaused()) {
+                    currentBuzzObject.play();
+                    
+                }
+            }              
+                
+        };
+    
+        SongPlayer.pause = function (song){
+            currentBuzzObject.pause();
+            song.playing = false;
+        }
+        
+    return SongPlayer;
+    
+    }
+
+    angular
+        .module('blocJams')
+        .factory('SongPlayer', SongPlayer);
+})();
