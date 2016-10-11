112fc6e85a97adbbf58f540857da72c3d7b59d7d
Jason Clegg <jgcdeveloper@gmail.com>
2016-10-11 09:49:10 +0500

    Checkpoint 08 Complete - Added script/link for PlayerBarControl.js and wrote functionaliy for Player Bar

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,26 +1,28 @@
 (function() {
-    function SongPlayer() {
+    function SongPlayer(Fixtures) {
                
         /**
         * @desc SongPlayer constructor. Return for access to environment
         * @type {Object}
         */
-        var SongPlayer = {},
+        var SongPlayer = {};
             
+        
         /**
-        * @desc currentSong selected is stored here
+        * @desc gets the current album stored in fixtures and stores it to a local variable
         * @type {Object}
         */
-        _currentSong = null,
+        var _currentAlbum = Fixtures.getAlbum();
+        
             
         /**
         * @desc Buzz object audio file for use with the buzzMusic API
         * @type {Object}
         */
-        _currentBuzzObject = null;
+        var _currentBuzzObject = null;
         
         /*
-        * @function setSong (private)
+        * @function _setSong (private)
         * @desc Stops currently playing song and loads new audio file as currentBuzzObject
         * @param {Object} song
         */
@@ -28,7 +30,7 @@
             
             if (_currentBuzzObject) {
                 _currentBuzzObject.stop();
-                _currentSong.playing = null;
+                SongPlayer.currentSong.playing = null;
             }
  
             _currentBuzzObject = new buzz.sound(song.audioUrl, {
@@ -36,42 +38,56 @@
                 preload: true
             });
  
-            _currentSong = song;
+            SongPlayer.currentSong = song;
         };
-        
+               
         /*
-        * @function playSong (private)
+        * @function _playSong (private)
         * @desc Starts song playing and updates song key of playing to value of true
         * @param {Object} song
         */
         var _playSong = function(song){
             _currentBuzzObject.play(song);
             song.playing = true;
-        }
+        };
+        
+        /*
+        * @function _getSongIndex (private)
+        * @desc Starts song playing and updates song key of playing to value of true
+        * @param {Object} song
+        * @return The index of the current song that is playing
+        */
+        var _getSongIndex = function(song){
+            return _currentAlbum.songs.indexOf(song);
+        };
+        
+        
+        /**
+        * @desc currentSong is stored here in public object SongPlayer. Initialized as null.
+        * @type {Object}
+        */
+        SongPlayer.currentSong = null;
         
         /*
         * @method .play (public)
-        * @desc Starts our _currentBuzzObject to play
+        * @desc Starts our _currentBuzzObject to play. 
         * @param {Object} song
         */        
         SongPlayer.play = function(song){
             
-            if (_currentSong !== song) {    
+            song = song || SongPlayer.currentSong;
+            
+            if (SongPlayer.currentSong !== song) {    
                 _setSong(song);
                 _playSong(song);
-                
-                //currentBuzzObject.play();
-                //song.playing = true;
-            
-            } else if(currentSong === song) {
+                            
+            } else if(SongPlayer.currentSong === song) {
             
                 if (_currentBuzzObject.isPaused()) {
                     _playSong(song);
-                    //currentBuzzObject.play();
-                    
+                   
                 }
             }              
-                
         };
     
         /*
@@ -80,9 +96,32 @@
         * @param {Object} song
         */  
         SongPlayer.pause = function (song){
+            
+            song = song || SongPlayer.currentSong;
             _currentBuzzObject.pause();
             song.playing = false;
         }
+        
+        /*
+        * @method .previous (public)
+        * @desc Decrements our song index by 1
+        * @param {Object} song
+        */  
+        SongPlayer.previous = function() {
+            var currentSongIndex = _getSongIndex(SongPlayer.currentSong);
+            currentSongIndex--;
+        
+            if(currentSongIndex < 0) {
+                _currentBuzzObject.stop();
+                SongPlayer.currentSong.playing = null;
+            } else {
+                var song = _currentAlbum.songs[currentSongIndex];
+                _setSong(song);
+                _playSong(song);
+            }
+        
+        };
+        
         
     return SongPlayer;
     
