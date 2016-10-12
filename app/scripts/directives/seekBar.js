1cc684bd1ce2d24e06827ccdd86aa02325adc695
Jason Clegg <jgcdeveloper@gmail.com>
2016-10-13 01:10:41 +0500

    Completed Assignment CP09 - Updated ThumbStyle functionality to update on click and mousedrag

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -31,10 +31,15 @@
                     return percent + "%";
                 };
  
+                scope.thumbStyle = function() {
+                    return {left: percentString()}  
+                };
+                
                 scope.fillStyle = function() {
                     return {width: percentString()};
                 };
-        
+                
+                                
                 scope.onClickSeekBar = function(event) {
                     var percent = calculatePercent(seekBar, event);
                     scope.value = percent * scope.max;
@@ -44,9 +49,11 @@
                 scope.trackThumb = function() {
                     $document.bind('mousemove.thumb', function(event) {
                         var percent = calculatePercent(seekBar, event);
+                                            
                         scope.$apply(function() {
                             scope.value = percent * scope.max;
                         });
+                        
                     });
  
                     $document.bind('mouseup.thumb', function() {
